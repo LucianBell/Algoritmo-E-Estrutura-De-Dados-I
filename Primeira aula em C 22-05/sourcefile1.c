c64938e8fb8f9e5773c0cf724906d5d2611a0f9f
Lucian Bellini <64754204+LucianBell@users.noreply.github.com>
2023-05-22 21:56:48 -0300

    First class using C language

---
+++ Primeira aula em C 22-05/sourcefile1.c
@@ -0,0 +1,19 @@
+//Incluir bibliotecas que tem funções e ferramentas já criadas por terceiros
+        //Arquivo de cabeçalho
+#include <stdio.h>
+#include <stdlib.h>
+
+//Função principal do código fonte
+int main()
+{
+    int n1, n2, soma;
+    n1 = 10;
+    n2 = 20;
+
+    soma = n1 + n2;
+
+                //Lugar para mostrar variavel -> caracter especial (d de digit)
+    printf("Soma: %d", soma);
+
+    return 0;
+}
