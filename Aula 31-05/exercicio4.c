b3d6e625fc200c82b231f9bbedb6993427f12feb
Lucian Bellini <64754204+LucianBell@users.noreply.github.com>
2023-06-06 20:04:49 -0300

    Adding exercise 4

--- Aula 31-05/exercicio4.c
+++ Aula 31-05/exercicio4.c
@@ -1,7 +1,6 @@
 /*
-4)   
-Escreva um algoritmo que gere a
-tabuada a partir de um número informado pelo usuário.
+4) Faça um algoritmo que leia um número n que indica quantos valores devem ser lidos a seguir.
+Para cada número lido, mostre uma tabela contendo o valor lido e o fatorial deste valor.
 */
 
 #include <stdio.h>
@@ -9,17 +8,32 @@ tabuada a partir de um número informado pelo usuário.
 
 int main()
 {
-    int num, ctrl;
-    ctrl = 1;
+    int numsALer, num, controle, controle1, fatorial;
 
-    printf("Insira um número para ver sua tabuada: ");
-    scanf("%d", &num);
+    printf("Insira quantos valores você vai informar: ");
+    scanf("%d", &numsALer);
 
-    while (ctrl <= 10)
+    controle = 0;
+
+    while (controle < numsALer)
     {
-        printf("%d x %d = %d\n", ctrl, num, (num * ctrl));
-        ctrl++;
+        printf("Insira o valor desejado: ");
+        scanf("%d", &num);
+        controle1 = (num - 1);
+        fatorial = num;
+        while (controle1 != 1)
+        {
+            fatorial = (fatorial * (controle1));
+            controle1--;
+        }
+        printf("**********************\n");
+        printf("*NUMERO******FATORIAL*\n");
+        printf("**********************\n");
+        printf("*  %d     *     %d   *\n", num, fatorial);
+        printf("**********************\n");
+        controle++;
     }
+    printf("Finalizando programa...");
 
     return 0;
 }
