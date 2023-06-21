a9123649498d66c57b399b6d6a941acab07baeea
Lucian Bellini <64754204+LucianBell@users.noreply.github.com>
2023-06-21 00:05:17 -0300

    Adding exercise 9

---
+++ Aula 12-06/exercicio9.c
@@ -0,0 +1,19 @@
+/*
+Faça um algoritmo que leia um vetor com 8(oito) números inteiros, calcule e mostre dois vetores
+resultantes. O primeiro vetor deve conter os números positivos; o segundo deve conter os números
+negativos. Cada vetor resultante vai ter, no máximo, oito posições, que poderão não ser
+completamente utilizadas. Caso um dos vetores resultantes (de negativos ou de positivos) não possua
+elementos, uma mensagem deverá ser exibida. (Por exemplo: “vetor de negativos vazio” e/ou “vetor
+de positivos vazio”).
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int vetorA[8];
+
+    printf("Insira os valores do vetor A\n");
+    return 0;
+}
